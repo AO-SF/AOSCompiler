a5a6f3e6c6edd92a7a63fa10cd5b4b9f19e0e154
DJW <asd@example.com>
2020-04-20 21:39:05 +0100

    Removed nearley stuff and started writing own tokenizer

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-const nearley = require("nearley");
+import { Tokenizer } from './tokenizer'
 
 if (process.argv.length!=4) {
 	console.log("bad arguments"); // TODO: improve this
@@ -12,12 +12,10 @@ let fs=require('fs');
 let path=require('path');
 const inputData = fs.readFileSync(path.resolve(__dirname, inputPath), 'utf8')
 
-// Create a Parser object from our grammar.
-const grammar = require("../src/grammar1.js");
-const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
+let tokens=Tokenizer.tokenize(inputData);
+if (tokens.length==0) {
+	console.log("Error: could not tokenize\n");
+	process.exit(0);
+}
 
-// Parse something!
-parser.feed(inputData);
-
-// parser.results is an array of possible parsings.
-console.log(parser.results);
+console.log(tokens);
---
+++ src/tokenizer.ts
@@ -0,0 +1,82 @@
+export class Token {
+	public constructor(public text: string, public lineNum: number, public columnNum: number) {
+	}
+}
+
+export class Tokenizer {
+	static operators: string[]=['==','!=','<=','>=','<','>','=','+','-','*','/','&','|','^','!',';'];
+
+	static tokenize(input: string):Token[] {
+		let tokens: Token[] = [];
+		let lineNum=1;
+		let lineStartOffset=0;
+
+		for(let i=0; i<input.length; ++i) {
+			let c=input[i];
+			let sub=input.substr(i);
+			let columnNum=i-lineStartOffset+1;
+
+			// Whitespace?
+			if (this.charIsWhitespace(c)) {
+				// If newline then increment line count and reset column
+				if (c=='\n') {
+					++lineNum;
+					lineStartOffset=i+1;
+				}
+				continue;
+			}
+
+ 			// Literal?
+ 			if (this.charIsLiteral(c)) {
+ 				// Consume as many characters as possible
+ 				let text=c;
+ 				while(i+1<input.length) {
+ 					let c2=input[i+1];
+ 					if (!this.charIsLiteral(c2))
+ 						break;
+ 					text+=c2;
+ 					++i;
+ 				}
+ 				tokens.push(new Token(text, lineNum, columnNum));
+
+ 				continue;
+ 			}
+
+ 			// Operator?
+ 			let j;
+ 			for(j=0; j<this.operators.length; ++j) {
+ 				if (sub.startsWith(this.operators[j])) {
+ 					tokens.push(new Token(this.operators[j], lineNum, columnNum));
+ 					i+=this.operators[j].length-1;
+ 					break;
+ 				}
+ 			}
+ 			if (j<this.operators.length)
+ 				continue;
+
+ 			// Unexpected character
+ 			console.log("Could not tokenize: unexpected character '"+c+"' (line "+lineNum+", column "+columnNum+")");
+ 			return [];
+		}
+
+		return tokens;
+	}
+
+	static charIsLiteral(char: string):boolean {
+		if (char.charCodeAt(0)>='0'.charCodeAt(0) && char.charCodeAt(0)<='9'.charCodeAt(0))
+			return true;
+		if (char.charCodeAt(0)>='a'.charCodeAt(0) && char.charCodeAt(0)<='z'.charCodeAt(0))
+			return true;
+		if (char.charCodeAt(0)>='A'.charCodeAt(0) && char.charCodeAt(0)<='Z'.charCodeAt(0))
+			return true;
+		if (char=='_')
+			return true;
+		return false;
+	}
+
+	static charIsWhitespace(char: string):boolean {
+		if (char==' ' || char=='\t' || char=='\r' || char=='\n')
+			return true;
+		return false;
+	}
+}
