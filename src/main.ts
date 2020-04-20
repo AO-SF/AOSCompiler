e9445410fc495e3933aded03e29558505479c41e
DJW <asd@example.com>
2020-04-20 21:51:58 +0100

    Refactor main.ts and add call to Parser.parse

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
+import { Parser } from './parser'
 import { Tokenizer } from './tokenizer'
 
+// Process arguments
 if (process.argv.length!=4) {
 	console.log("bad arguments"); // TODO: improve this
 	process.exit(1);
@@ -8,14 +10,25 @@ if (process.argv.length!=4) {
 const inputPath=process.argv[2];
 const outputPath=process.argv[3];
 
+// Load input file data
 let fs=require('fs');
 let path=require('path');
 const inputData = fs.readFileSync(path.resolve(__dirname, inputPath), 'utf8')
 
+// Tokenize
 let tokens=Tokenizer.tokenize(inputData);
-if (tokens.length==0) {
+if (tokens===null) {
 	console.log("Error: could not tokenize\n");
 	process.exit(0);
 }
 
+// Parse
+let ast=Parser.parse(tokens);
+if (ast===null) {
+	console.log("Error: could not parse\n");
+	process.exit(0);
+}
+
+// Temp debugging
 console.log(tokens);
+console.log(ast);
