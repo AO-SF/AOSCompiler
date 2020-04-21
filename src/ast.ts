bca88aeb2852707b166a4ff5aaeaea49cdf65b7a
DJW <asd@example.com>
2020-04-21 01:53:30 +0100

    Created basic ast class

--- src/ast.ts
+++ src/ast.ts
@@ -1,4 +1,29 @@
-export class Ast {
-	public constructor() {
+import { Token } from './tokenizer';
+
+export enum AstNodeType {
+	Root,
+	Definition, // either variable or function definition
+	Type,
+	Name,
+	VariableDefinition,
+	FunctionDefinition,
+	FunctionDefinitionArguments,
+};
+
+export class AstNode {
+	public parent: AstNode | null = null;
+	public children: AstNode[] = [];
+
+	public tokens: Token[] = [];
+
+	public constructor(public type: AstNodeType) {
+	}
+
+	public createChild(type: AstNodeType): AstNode {
+		let child=new AstNode(type);
+		child.parent=this;
+		this.children.push(child);
+
+		return child;
 	}
 }
