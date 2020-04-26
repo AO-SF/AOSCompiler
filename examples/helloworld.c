8b1f13673b524da779dfeb01c09722ccc9049279
DJW <asd@example.com>
2020-04-27 00:19:24 +0100

    Added helloworld.c example to test strings

---
+++ examples/helloworld.c
@@ -0,0 +1,12 @@
+// Print 'Hello world!\n'
+
+void put_str(uint8_t *str) {
+	// Use puts0 library function
+	asm "requireend lib/std/io/fput.s";
+	asm "$str\nload16 r0 r0\ncall puts0";
+}
+
+uint16_t main() {
+	put_str("Hello world!\n");
+	return 0;
+}
