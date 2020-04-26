2668b46ed35662d873d1e040fd6b2e5ccdccb0b2
DJW <asd@example.com>
2020-04-26 22:24:07 +0100

    Updated fib.c to use new inline asm variable substitution

--- examples/fib.c
+++ examples/fib.c
@@ -1,13 +1,19 @@
-
 // Print Fibonacci sequence
 
+void put_char(uint8_t c) {
+	// Use putc0 library function
+	asm "requireend lib/std/io/fput.s";
+	asm "$c\nload8 r0 r0\ncall putc0";
+}
+
 void put_dec(uint16_t x) {
-	// TODO: this properly (temp hack follows)
-	x; // load x into r0
-	asm "debug"; // debug all registers so we can inspect r0
+	// Print x using putdec library function
+	asm "requireend lib/std/io/fputdec.s";
+	asm "$x\nload16 r0 r0\ncall putdec";
 }
 
 uint16_t main() {
+	// Initialise and print first value
 	uint16_t lower;
 	uint16_t upper;
 
@@ -15,14 +21,23 @@ uint16_t main() {
 	upper=1;
 
 	put_dec(lower);
+
+	// Loop to print subsequent values
 	while(lower<40000) {
+		// Print separator followed by value
+		put_char(44); // ','
+		put_char(32); // ' '
 		put_dec(upper);
 
+		// Execute single Fibonacci step
 		uint16_t sum;
 		sum=lower+upper;
 		lower=upper;
 		upper=sum;
 	}
+
+	// Print newline
+	put_char(10);
 
 	return 0;
 }
