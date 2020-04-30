38e56407cb28599edbbcb63ce5b9bc278d6e5864
DJW <asd@example.com>
2020-04-30 02:18:07 +0100

    Minor improvements to echo.c example

--- examples/echo.c
+++ examples/echo.c
@@ -5,14 +5,22 @@ void put_str(uint8_t *str) {
 }
 
 uint16_t main(uint8_t argc, uint8_t **argv) {
+	// Loop over arguments (excluding the first)
 	uint8_t i;
-	i=0;
+	i=1;
 	while(i<argc) {
-		if (i>0) {
+		// If there has been an argument printed before this then add a space before we print the next one.
+		if (i>1) {
 			put_str(" ");
 		}
+
+		// Print current argument and increment i to handle next instruction
 		put_str(argv[i]);
 		i=i+1;
 	}
+
+	// Add newline to terminate output
+	put_str("\n");
+
 	return 0;
 }
