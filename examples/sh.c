d42fe542a16ffbe99367ec106282c8de688cb54f
DJW <asd@example.com>
2020-05-03 21:35:53 +0100

    Added many syscall wrappers to sh.c example, such as exit and fork

--- examples/sh.c
+++ examples/sh.c
@@ -59,6 +59,12 @@ uint8_t runFd(uint8_t fd, uint8_t interactiveMode) {
 // Library functions
 ////////////////////////////////////////////////////////////////////////////////
 
+void exit(uint16_t status) {
+	asm "$status\nload16 r1 r0";
+	asm "mov r0 SyscallIdExit";
+	asm "syscall";
+}
+
 // reads up to and including first newline, always null-terminates buf (potentially to be 0 length if could not read)
 // returns number of bytes read
 uint16_t fgets(uint8_t fd, uint16_t offset, uint8_t *buf, uint16_t len) {
@@ -143,4 +149,35 @@ uint8_t *strchr(uint8_t *str, uint8_t c) {
 	asm "store16 r0 r1";
 
 	return ret;
+}
+
+uint8_t fork() {
+	asm "mov r0 SyscallIdFork";
+	asm "syscall";
+
+	uint8_t ret;
+	asm "push8 r0";
+	asm "$ret\ndec r0";
+	asm "pop8 r1";
+	asm "store8 r0 r1";
+
+	return ret;
+}
+
+void waitpid(uint8_t pid, uint16_t timeoutSeconds) {
+	asm "$timeoutSeconds\nload16 r0 r0\npush16 r0";
+	asm "$pid\ndec2 r0\nload8 r1 r0";
+	asm "pop16 r2";
+	asm "mov r0 SyscallIdWaitPid";
+	asm "syscall";
+}
+
+void exec(uint8_t argc, uint8_t *argv, uint8_t searchFlag) {
+	asm "$searchFlag\nload8 r0 r0\npush8 r0";
+	asm "$argv\ndec1 r0\nload16 r0 r0\npush16 r0";
+	asm "$argc\ndec3 r0\nload8 r1 r0";
+	asm "pop16 r2";
+	asm "pop8 r3";
+	asm "mov r0 SyscallIdExec";
+	asm "syscall";
 }
